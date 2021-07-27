81e9109cc1ddf5fa0bb5f228848e5db1204c89c0
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-27 19:56:19 +0700

    Add project files.

---
+++ SK-Lib-cmath-nearbyint-Bagian3__CPP/Source.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	double x = 5.1;
+
+	cout << "Value of x = " << x << endl;
+	cout << "Round off value of x = " << nearbyint(x);
+
+	_getch();
+	return 0;
+}
